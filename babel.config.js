f3d415c11bddac10852896571644278e9c3fc475
Harshit Raj <harshitr20@iitk.ac.in>
2021-09-20 18:06:11 +0530

    Configure Bable, Add module resolver

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,21 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["."],
+          extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
+          alias: {
+            components: "./src/components",
+            constant: "./src/constant",
+            styles: "./src/styles",
+            utils: "./src/utils",
+          },
+        },
+      ],
+    ],
   };
 };
