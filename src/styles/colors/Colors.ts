a49d0645cb101325a66e4e2b4834c6bd60dd0af9
Bhuvan Singla <bhuvansingla2000@gmail.com>
2021-09-21 19:13:32 +0530

    Lint Fix

--- src/styles/colors/Colors.ts
+++ src/styles/colors/Colors.ts
@@ -1,13 +1,13 @@
 export default {
-    mainbg : "#002E6E",
-    teal: "#329691", 
-    grey: "#C4C4C4", // 50%
-    link: "#082452",
-    white: "#FFFFFF",
-    black: "#000000",
-    blue: "#305A90", 
-    red: "#E4592D",
-    green: "#12AC3C",
-    greenbg: "#D5FD56",
-    yellow: "#FECC18",
+	mainbg : "#002E6E",
+	teal: "#329691", 
+	grey: "#C4C4C4", // 50%
+	link: "#082452",
+	white: "#FFFFFF",
+	black: "#000000",
+	blue: "#305A90", 
+	red: "#E4592D",
+	green: "#12AC3C",
+	greenbg: "#D5FD56",
+	yellow: "#FECC18",
 };
