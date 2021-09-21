dfccfa77f1491d13bcaea0fcd7fd69c0981be03d
Bhuvan Singla <bhuvansingla2000@gmail.com>
2021-09-21 19:20:20 +0530

    Lint config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,31 @@
 module.exports = {
-	"env": {
-		"es2021": true,
-		"node": true
+	env: {
+		es2021: true,
+		node: true,
 	},
-	"extends": [
+	extends: [
 		"eslint:recommended",
 		"plugin:react/recommended",
-		"plugin:@typescript-eslint/recommended"
+		"plugin:@typescript-eslint/recommended",
 	],
-	"parser": "@typescript-eslint/parser",
-	"parserOptions": {
-		"ecmaFeatures": {
-			"jsx": true
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
 		},
-		"ecmaVersion": 12,
-		"sourceType": "module"
+		ecmaVersion: 12,
+		sourceType: "module",
 	},
-	"plugins": [
-		"react",
-		"@typescript-eslint"
-	],
-	"rules": {
-		"indent": [
-			"error",
-			"tab"
-		],
-		"linebreak-style": [
-			"error",
-			"unix"
-		],
-		"quotes": [
-			"error",
-			"double"
-		],
-		"semi": [
-			"error",
-			"always"
-		]
-	}
+	plugins: ["react", "@typescript-eslint"],
+	rules: {
+		indent: ["error", "tab"],
+		"linebreak-style": ["error", "unix"],
+		quotes: ["error", "double"],
+		semi: ["error", "always"],
+	},
+	settings: {
+		react: {
+			version: "detect",
+		},
+	},
 };
