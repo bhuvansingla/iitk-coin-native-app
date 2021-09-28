6fed0840849dedeba01f280b72ebcbb3c30eba99
Harshit Raj <harshitr20@iitk.ac.in>
2021-09-28 17:14:52 +0530

    Add dependency for Bottom sheet

--- babel.config.js
+++ babel.config.js
@@ -1,21 +1,22 @@
 module.exports = function(api) {
 	api.cache(true);
 	return {
-		presets: ["babel-preset-expo"],
-		plugins: [
-			[
-				"module-resolver",
-				{
-					root: ["."],
-					extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
-					alias: {
-						components: "./src/components",
-						constant: "./src/constant",
-						styles: "./src/styles",
-						utils: "./src/utils",
-					},
-				},
-			],
-		],
-	};
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["."],
+          extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
+          alias: {
+            components: "./src/components",
+            constant: "./src/constant",
+            styles: "./src/styles",
+            utils: "./src/utils",
+          },
+        },
+      ],
+      "react-native-reanimated/plugin",
+    ],
+  };
 };
