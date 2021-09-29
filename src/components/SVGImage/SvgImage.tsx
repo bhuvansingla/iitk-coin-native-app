8c4003246208787613ae76cd33b1d679cf0d317c
Harshit Raj <harshitr20@iitk.ac.in>
2021-09-29 22:10:30 +0530

    Fix as per Review
    Fix Indendation, formatting, file names, and new lines

--- src/components/SVGImage/SvgImage.tsx
+++ src/components/SVGImage/SvgImage.tsx
@@ -12,30 +12,34 @@ interface Prop {
 
 const SImage: React.FC<Prop> = (props) => {
 	const { name, size, style } = props;
+  
 	// SVG Tag
 	let SVG = source.Crow;
 
 	const height = size || "100%";
 
-	
 	if (name === "Crow") {
 		return <SVG height={height} style={[styles.default, styles.crow, style]} />;
 	}
 
-	if (name === "CoinTypographyLogo") {	
+	if (name === "CoinTypographyLogo") {
 		SVG = source.CoinTypographyLogo;
 		return (
-      <SVG
-        height={height}
-        style={[styles.default, styles.coinTypographyLogo, style]}
-      />
-    );
-	} if(name === "CoinLogo") {	
-		SVG = source.CoinLogo;
-		return <SVG height={height} style={[styles.default, styles.coinLogo, style]} />;
-	} else {
-		return <View/>;
+			<SVG
+				height={height}
+				style={[styles.default, styles.coinTypographyLogo, style]}
+			/>
+		);
 	}
+
+	if (name === "CoinLogo") {
+		SVG = source.CoinLogo;
+		return (
+			<SVG height={height} style={[styles.default, styles.coinLogo, style]} />
+		);
+	}
+
+	return <View />;
 };
 
 export default SImage;
--- src/components/SVGImage/image.styles.ts
+++ src/components/SVGImage/image.styles.ts
@@ -4,28 +4,27 @@ import { Layout } from "constant";
 const height = Layout.screen.height;
 
 const styles = StyleSheet.create({
-  default: {
-    flex: 1,
-  },
-  crow: {
-    position: "absolute",
-    top: 0,
-    right: 0,
-    marginRight: 0,
-    paddingRight: 0,
-    alignItems: "flex-end",
-    justifyContent: "flex-start",
-    height: height * 0.4,
-  },
-  coinTypographyLogo: {
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  coinLogo: {
-    // alignItems: "flex-end",
-    justifyContent: "center",
-  },
+	default: {
+		flex: 1,
+	},
+	crow: {
+		position: "absolute",
+		top: 0,
+		right: 0,
+		marginRight: 0,
+		paddingRight: 0,
+		alignItems: "flex-end",
+		justifyContent: "flex-start",
+		height: height * 0.4,
+	},
+	coinTypographyLogo: {
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	coinLogo: {
+		// alignItems: "flex-end",
+		justifyContent: "center",
+	},
 });
-		
 
 export default styles;
