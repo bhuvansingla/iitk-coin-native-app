592df17799c777ae1cae03059a42249b2c099f6b
Bhuvan Singla <bhuvansingla2000@gmail.com>
2021-10-02 15:27:13 +0530

    Add redeem status and tax to schema

--- src/api/transaction-history.ts
+++ src/api/transaction-history.ts
@@ -15,15 +15,25 @@ interface TransferHistory {
 	Type: TransactionType.TRANSFER
 	TimeStamp: number
 	Amount: number
+	Tax: number
 	FromRollNo: number
 	ToRollNo: number
 	Remarks: string
 }
+
+enum RedeemStatus {
+	PENDING = "PENDING",
+	APPROVED = "APPROVED",
+	CANCELLED = "CANCELLED",
+	DISAPPROVED = "DISAPPROVED"
+}
+
 interface RedeemHistory {
 	Type: TransactionType.REDEEM
 	TimeStamp: number
 	Amount: number
 	Remarks: string
+	Status: RedeemStatus
 }
 
 type TransactionHistory = RewardHistory | RedeemHistory | TransferHistory;
@@ -40,7 +50,7 @@ interface TransactionHistoryResponse {
 
 // example -- delete later.
 const rewardHistoryEntry: RewardHistory = { Amount: 100, TimeStamp: 100, Type: TransactionType.REWARD, Remarks: "" };
-const redeemHistoryEntry: RedeemHistory = { Amount: 100, TimeStamp: 100, Type: TransactionType.REDEEM, Remarks: "" };
-const transferHistoryEntry: TransferHistory = { Amount: 100, TimeStamp: 100, Type: TransactionType.TRANSFER, FromRollNo: 180199, ToRollNo: 180199, Remarks: "" };
+const redeemHistoryEntry: RedeemHistory = { Amount: 100, TimeStamp: 100, Type: TransactionType.REDEEM, Remarks: "", Status: RedeemStatus.APPROVED };
+const transferHistoryEntry: TransferHistory = { Amount: 100, TimeStamp: 100, Type: TransactionType.TRANSFER, FromRollNo: 180199, ToRollNo: 180199, Remarks: "", Tax: 10 };
 const transactionHistory: TransactionHistory[] = [rewardHistoryEntry, transferHistoryEntry, redeemHistoryEntry];
 transactionHistory.forEach(transaction => (console.log(transaction.Type, transaction.TimeStamp)));
