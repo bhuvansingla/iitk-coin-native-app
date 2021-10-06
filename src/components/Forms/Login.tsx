2ae69f1f493cb8df74e174cbb016968c61b50167
Bhuvan Singla <bhuvansingla2000@gmail.com>
2021-10-06 21:33:40 +0530

    Connect signin button to login callback

--- src/components/Forms/Login.tsx
+++ src/components/Forms/Login.tsx
@@ -2,11 +2,17 @@ import React, { useState } from "react";
 import { View } from "react-native";
 import { TextInput, Button } from "components";
 import { LABELS, LENGTH } from "constant";
-
+import { LoginParams } from "api/auth";
+import { loginCallback } from "callbacks/login";
+import { useDispatch } from "react-redux";
+import { setCurrentScreen, setIsAuthenticated } from "redux-store/actions";
+import { ScreenType } from "screens/screen.types";
 const LoginForm: () => JSX.Element = () => {
 
 	const [password, setPassword] = useState<string>("");
 	const [rollNo, setRollNo] = useState<string>("");
+
+	const dispatch = useDispatch();
 
 	const onChangePassword = (password: string) => {
 		if (password.length < LENGTH.NAME) {
@@ -18,7 +24,17 @@ const LoginForm: () => JSX.Element = () => {
 		if (rollNo.length < LENGTH.ROLL_NO) {
 			setRollNo(rollNo);
 		}
-		
+
+	};
+
+	const onPressSignin = () => {
+		const loginParams: LoginParams = { Rollno: rollNo, Password: password };
+		loginCallback(loginParams).then((success) => {
+			if (success) {
+				dispatch(setIsAuthenticated(true));
+				dispatch(setCurrentScreen(ScreenType.HOME));
+			}
+		});
 	};
 
 	// Login form
@@ -41,7 +57,7 @@ const LoginForm: () => JSX.Element = () => {
 				value={password}
 			/>
 
-			<Button title= {LABELS.SIGNIN_BUTTON_TEXT} onPress={() => {console.log(rollNo);}}/>
+			<Button title={LABELS.SIGNIN_BUTTON_TEXT} onPress={() => onPressSignin()} />
 
 		</View>
 
