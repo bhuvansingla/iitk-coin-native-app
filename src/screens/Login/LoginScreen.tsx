e8712e7eb23c577e03b0fe349f880fff9bf71d80
Bhuvan Singla <bhuvansingla2000@gmail.com>
2021-10-06 20:52:18 +0530

    Connect signin and signup screen with each other

--- src/screens/Login/LoginScreen.tsx
+++ src/screens/Login/LoginScreen.tsx
@@ -1,35 +1,40 @@
 import React from "react";
+
+import { useDispatch } from "react-redux";
+import { setCurrentScreen } from "redux-store/actions";
+
 import { View } from "react-native";
 import { Text } from "components";
-import styles from "../screen.styles";
-import { LABELS} from "constant";
 import LoginForm from "components/Forms/Login";
+
+import styles from "../screen.styles";
+
+import { LABELS } from "constant";
+import { ScreenType } from "screens/screen.types";
+
 
 const LoginScreen: () => JSX.Element = () => {
 
-	// Login form
-	const loginPage = (
+	const dispatch = useDispatch();
 
+	const onPressFooter = () => {
+		dispatch(setCurrentScreen(ScreenType.SIGNUP));
+	};
+
+	return (
 		<View style={styles.contentContainer}>
 
 			<Text.Heading title={LABELS.SIGNIN_FORM_TITLE} />
 
 			<View style={styles.formContainer}>
 
-				<LoginForm/>
-				<Text.Footer title={LABELS.CREATE_WALLET_FOOTER} link={LABELS.CREATE_WALLET_LINK} onPress={() => {console.log("Create Wallet");}} />
+				<LoginForm />
+
+				<Text.Footer title={LABELS.CREATE_WALLET_FOOTER} link={LABELS.CREATE_WALLET_LINK} onPress={() => onPressFooter()} />
 
 			</View>
-
 		</View>
-		
 	);
-
-	// render
-	return (
-		loginPage
-	);
-
 };
 
 export default LoginScreen;
--- src/screens/Signup/SignupScreen.tsx
+++ src/screens/Signup/SignupScreen.tsx
@@ -1,34 +1,40 @@
 import React from "react";
+
+import { useDispatch } from "react-redux";
+import { setCurrentScreen } from "redux-store/actions";
+
 import { View } from "react-native";
 import { Text } from "components";
+import SignupForm from "components/Forms/SignUp";
+
 import styles from "../screen.styles";
+
 import { LABELS } from "constant";
-import SignUpForm from "components/Forms/SignUp";
+import { ScreenType } from "screens/screen.types";
+
 
 const SignupScreen: () => JSX.Element = () => {
 
-	// Sign Up form
-	const signUpPage = (
+	const dispatch = useDispatch();
 
+	const onPressFooter = () => {
+		dispatch(setCurrentScreen(ScreenType.LOGIN));
+	};
+
+	return (
 		<View style={styles.contentContainer}>
-            
+
 			<Text.Heading title={LABELS.SIGNUP_FORM_TITLE} />
 
 			<View style={styles.formContainer}>
 
-				<SignUpForm />
-				<Text.Footer title={LABELS.SIGNIN_FOOTER} link={LABELS.SIGNIN_LINK} onPress={() => { console.log("Go to Sign In Page"); }} />
-			
+				<SignupForm />
+
+				<Text.Footer title={LABELS.SIGNIN_FOOTER} link={LABELS.SIGNIN_LINK} onPress={() => onPressFooter()} />
+
 			</View>
-			
 		</View>
 	);
-
-	// render
-	return (
-		signUpPage
-	);
-
 };
 
 export default SignupScreen;
