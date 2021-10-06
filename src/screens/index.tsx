f7b510bee3ae465394bef9e329e996cbc6cb7896
Bhuvan Singla <bhuvansingla2000@gmail.com>
2021-10-06 21:01:03 +0530

    Add callbacks to config files

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -5,19 +5,28 @@ import SignupScreen from "./Signup/SignupScreen";
 import { ScreenType } from "./screen.types";
 import { useSelector } from "react-redux";
 import { AppState } from "redux-store/reducers";
-
+import { View } from "react-native";
 
 const RootScreen: () => JSX.Element = () => {
-	
+
 	const shrinkedHeader = <Header.Shrinked />;
 	const expandedHeader = <Header.Expanded />;
 
 	const currentScreen: ScreenType = useSelector((state: AppState) => state.display.currentScreen);
+	const isAuthenticated: boolean = useSelector((state: AppState) => state.auth.isAuthenticated);
 
 	return (
 		<BottomSheet expandedHeader={expandedHeader} shrinkedHeader={shrinkedHeader}>
-			{currentScreen === ScreenType.LOGIN && <LoginScreen />}
-			{currentScreen === ScreenType.SIGNUP && <SignupScreen />}
+			{!isAuthenticated ?
+				<React.Fragment>
+					{currentScreen === ScreenType.LOGIN && <LoginScreen />}
+					{currentScreen === ScreenType.SIGNUP && <SignupScreen />}
+				</React.Fragment>
+				:
+				<React.Fragment>
+					{currentScreen === ScreenType.HOME && "Logged In"}
+				</React.Fragment>
+			}
 		</BottomSheet>
 	);
 };
