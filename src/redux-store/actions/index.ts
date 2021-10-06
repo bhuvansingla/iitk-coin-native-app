9ca08d9f59d545ce11316efb97db8d3eb52aa35b
Bhuvan Singla <bhuvansingla2000@gmail.com>
2021-10-06 20:08:50 +0530

    Add reducer for maintaing screen state Fixes #61

--- src/redux-store/actions/index.ts
+++ src/redux-store/actions/index.ts
@@ -1,8 +1,9 @@
 import { setIsAuthenticated } from "redux-store/actions/auth";
 import { setName, setRollNo } from "redux-store/actions/user";
-
+import { setCurrentScreen } from "redux-store/actions/display";
 export {
 	setIsAuthenticated,
 	setName,
-	setRollNo
+	setRollNo,
+	setCurrentScreen,
 };
