f82b46a1c3ccf7b7552203748d996cdbc8b307ce
Bhuvan Singla <bhuvansingla2000@gmail.com>
2021-10-07 14:16:30 +0530

    Update form index.ts

--- src/components/Forms/index.tsx
+++ src/components/Forms/index.tsx
@@ -1,29 +1,5 @@
-import React from "react";
 import LoginForm from "./Login";
-import SignUpForm from "./SignUp";
-import OtpForm from "./Otp";
-import { View } from "react-native";
+import SignupForm from "./Signup";
+import VerifyOtpForm from "./VerifyOtp";
 
-interface Props {
-    form: "Login" | "SignUp" | "OTP"
-}
-
-const Forms: React.FC<Props> = ({ form }) => {
-	
-	if (form == "Login") {
-		return <LoginForm />;
-	}
-
-	if (form == "SignUp") {
-		return <SignUpForm />;
-	}
-
-	if (form == "OTP") {
-		return <OtpForm />;
-	}
-
-	return <View />;
-	
-};
-
-export default Forms;
+export default { SignupForm, LoginForm, VerifyOtpForm };
