7de0d567460ef8aa28c726e0b9e94497f482bec0
Harshit Raj <harshitr20@iitk.ac.in>
2021-10-11 01:03:53 +0530

    Add HistoryListItem

---
+++ src/components/Card/HistoryListItem/HistoryListItem.helper.tsx
@@ -0,0 +1,159 @@
+import React from "react";
+import { View } from "react-native";
+import Ripple from "react-native-material-ripple";
+import { FontAwesome, AntDesign } from "@expo/vector-icons";
+
+import { useSelector } from "react-redux";
+import { AppState } from "redux-store/reducers";
+
+import { Text } from "components";
+import { COLORS, FONT } from "styles";
+import { history } from "api";
+import { LABELS } from "constant";
+import styles from "./HistoryListItem.styles";
+
+interface DetailsListItemProps {
+	key: string;
+	title: string;
+	content: string | number;
+}
+
+interface ItemTitleProps {
+	onPress: () => void;
+	details: history.TransactionHistory;
+}
+
+const fontSize = FONT.SIZE.H3;
+type ItemType = "Sent" | "Received" | "Redeem" | "Reward" | undefined;
+
+const DetailsListItem: React.FC<DetailsListItemProps> = (props) => {
+	const { title, content } = props;
+
+	const TextStyle = {
+		white: true,
+		style: {
+			fontSize: 12,
+		},
+	};
+
+	const titleTextStyle = {
+		...TextStyle,
+		bold: true,
+	};
+
+	return (
+		<Text.Title {...TextStyle}>
+			<Text.Title {...titleTextStyle}>
+				{title}:{" "}
+			</Text.Title>
+			{content}
+		</Text.Title>
+	);
+};
+
+const DetailsList: React.FC<history.TransactionHistory> = (props) => {
+
+	const details: JSX.Element[] = [];
+
+	const { Type, TimeStamp, TxnID, Amount, Remarks } = props;
+
+	const timestamp = new Date(TimeStamp * 1000);
+	details.push(<DetailsListItem key="timestamp" title="Time Stamp" content={timestamp.toDateString()} />);
+	details.push(<DetailsListItem key="TxnID" title="Transaction ID" content={TxnID} />);
+
+	if (Type === history.TransactionType.TRANSFER) {
+		const { Tax, FromRollNo, ToRollNo } = props;
+		details.push(<DetailsListItem key={LABELS.HISTORY_FromRollNo} title={LABELS.HISTORY_FromRollNo} content={FromRollNo} />);
+		details.push(<DetailsListItem key={LABELS.HISTORY_ToRollNo} title={LABELS.HISTORY_ToRollNo} content={ToRollNo} />);
+		details.push(<DetailsListItem key={LABELS.HISTORY_Transfer_Amount} title={LABELS.HISTORY_Transfer_Amount} content={Amount} />);
+		details.push(<DetailsListItem key={LABELS.HISTORY_Tax} title={LABELS.HISTORY_Tax} content={Tax} />);
+	}
+
+	if (Type === history.TransactionType.REDEEM) {
+		const { Status } = props;
+		details.push(<DetailsListItem key={LABELS.HISTORY_Redeem_Amount} title={LABELS.HISTORY_Redeem_Amount} content={Amount} />);
+		details.push(<DetailsListItem key={LABELS.HISTORY_Status} title={LABELS.HISTORY_Status} content={Status} />);
+	}
+
+	if (Type === history.TransactionType.REWARD) {
+		details.push(<DetailsListItem key={LABELS.HISTORY_Reward_Amount} title={LABELS.HISTORY_Reward_Amount} content={Amount} />);
+	}
+	
+	details.push(<DetailsListItem key={LABELS.HISTORY_Remarks} title={LABELS.HISTORY_Remarks} content={Remarks} />);
+
+	return (
+		<View style={styles.detailsContainer}>
+			{details}
+		</View>
+	);
+};
+
+const ItemTitle: React.FC<ItemTitleProps> = (props) => {
+
+
+	// const userRollNo: string = useSelector((state: AppState) => state.user.rollNo);
+	const userRollNo = "200433";
+
+	const { onPress, details } = props;
+
+	let type: ItemType = undefined;
+	let image: JSX.Element = <View />;
+	let amount = details.Amount;
+
+	if (details.Type === history.TransactionType.REDEEM) {
+		type = "Redeem";
+		if(details.Status != history.RedeemStatus.APPROVED) {
+			amount = 0;
+		}
+		image = <AntDesign name="gift" size={fontSize * 1.7} color="white" />;
+	} else if (details.Type === history.TransactionType.REWARD) {
+		type = "Reward";
+		image = <FontAwesome name="trophy" size={fontSize * 1.7} color="black" />;
+	} else if (details.Type === history.TransactionType.TRANSFER) {
+		if (details.FromRollNo == userRollNo) {
+			type = "Sent";
+			image = <FontAwesome name="send" size={fontSize * 1.7} color="white" />;
+		} else if (details.ToRollNo == userRollNo) {
+			type = "Received";
+			amount = amount - details.Tax;
+			image = <FontAwesome name="send" size={fontSize * 1.7} color="black" />;
+		}
+	}
+
+	const credited = type === "Reward" || type === "Received";
+
+	return (
+		<View style={styles.rippleWrapper}>
+			<Ripple
+				rippleOpacity={0.25}
+				rippleSize={3500}
+				rippleColor={COLORS.GREY}
+				rippleDuration={1300}
+				rippleContainerBorderRadius={20}
+				onPress={onPress}
+				style={styles.ripple}
+			>
+				<View style={styles.container}>
+					<View style={[styles.left, {
+						backgroundColor: credited ? COLORS.GREEN_BG : COLORS.RED,
+						height: fontSize * 3,
+						width: fontSize * 3,
+						borderRadius: fontSize * 1.5,
+					}]}>
+						{image}
+					</View>
+					<View style={styles.text}>
+						<Text.Title bold style={{ fontSize }}> {type} </Text.Title>
+					</View>
+					<View style={styles.right}>
+						<Text.Title bold style={{ opacity: 0.9, color: credited ? COLORS.GREEN : COLORS.RED, fontSize }}>
+							{credited ? "+" : "-"}{amount}
+						</Text.Title>
+					</View>
+				</View>
+			</Ripple>
+		</View>
+	);
+};
+
+export { DetailsList, ItemTitle };
---
+++ src/components/Card/HistoryListItem/HistoryListItem.styles.ts
@@ -0,0 +1,88 @@
+import { StyleSheet } from "react-native";
+import { COLORS } from "styles";
+
+const styles = StyleSheet.create({
+	mainContainer: {
+		flex: 0,
+		padding: 5,
+	},
+	content: {
+		zIndex: -1,
+	},
+	rippleWrapper: {
+		minHeight: 115,
+		maxHeight: 200,
+	},
+	ripple: {
+		flex: 1,
+		flexDirection: "column",
+		alignItems: "center",
+
+		marginTop: 7,
+		marginBottom: 0,
+		paddingVertical: 20,
+		borderRadius: 20,
+
+		shadowRadius: 20,
+		shadowColor: COLORS.BLACK,
+		shadowOpacity: 0.5,
+		shadowOffset: { width: 0, height: 4 },
+		elevation: 4,
+
+		backgroundColor: COLORS.WHITE,
+		minHeight: 100,
+		maxHeight: 185,
+		width: "100%",
+	},
+	detailsContainer: {
+		zIndex: -1,
+		overflow: "hidden",
+
+		borderRadius: 20,
+		margin: 0,
+
+		paddingTop: 40,
+		paddingBottom: 5,
+		paddingLeft: 40,
+		paddingRight: 20,
+		top: -40,
+
+		shadowRadius: 20,
+		shadowColor: COLORS.BLACK,
+		shadowOpacity: 0.5,
+		shadowOffset: { width: 0, height: 4 },
+		elevation: 4,
+
+		backgroundColor: COLORS.DARK_TEAL,
+		width: "100%",
+	},
+	container: {
+		flex: 1,
+		flexDirection: "row",
+		justifyContent: "flex-start",
+		
+		zIndex: 2,
+		position: "relative",
+		paddingHorizontal: 20,
+		height: "100%",
+	},
+	left: {
+		zIndex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	right: {
+		flex: 1,
+		flexDirection: "row",
+		justifyContent: "flex-end",
+		alignItems: "center",
+	},
+	text: {
+		flex: 1,
+		flexDirection: "row",
+		justifyContent: "flex-start",
+		alignItems: "center",
+		opacity: 1,
+	},
+});
+export default styles;
