40ccc9d85ec31073f52594479acf38236f212528
Harshit Raj <harshitr20@iitk.ac.in>
2021-10-12 15:22:21 +0530

    Add coins to redux

--- src/redux-store/reducers/user.ts
+++ src/redux-store/reducers/user.ts
@@ -2,12 +2,14 @@ import { Action, ActionType } from "redux-store/types";
 
 interface State {
 	name: string,
-    rollNo: string
+    rollNo: string,
+	coins: number,
 }
 
 const initialState: State = {
 	name: "",
-	rollNo: ""
+	rollNo: "",
+	coins: 0,
 };
 
 const userReducer = (state: State = initialState, action: Action): State => {
@@ -16,6 +18,8 @@ const userReducer = (state: State = initialState, action: Action): State => {
 		return { ...state, name: action.payload };
 	case ActionType.UPDATE_ROLLNO:
 		return { ...state, rollNo: action.payload };
+	case ActionType.UPDATE_COINS:
+		return { ...state, coins: action.payload };
 	default:
 		return state;
 	}
--- src/redux-store/types/index.ts
+++ src/redux-store/types/index.ts
@@ -4,6 +4,7 @@ export enum ActionType {
 	USER_IS_AUTHENTICATED = "USER_IS_AUTHENTICATED",
 	UPDATE_NAME = "UPDATE_NAME",
 	UPDATE_ROLLNO = "UPDATE_ROLLNO",
+	UPDATE_COINS = "UPDATE_COINS",
 	SET_CURRENT_SCREEN = "SET_CURRENT_SCREEN"
 }
 
@@ -11,4 +12,5 @@ export type Action =
 	| { type: ActionType.USER_IS_AUTHENTICATED, payload: boolean }
 	| { type: ActionType.UPDATE_NAME, payload: string }
 	| { type: ActionType.UPDATE_ROLLNO, payload: string }
+	| { type: ActionType.UPDATE_COINS, payload: number }
 	| { type: ActionType.SET_CURRENT_SCREEN, payload: ScreenType };
