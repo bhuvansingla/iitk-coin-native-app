407ca26e4c581c958b5e727c98cee34324615e09
Harshit Raj <harshitr20@iitk.ac.in>
2021-10-13 14:01:18 +0530

    Rename DetailsListItem to ConfirmDetailsItem

--- src/components/Transfer/ConfirmDetails/ConfirmDetails.tsx
+++ src/components/Transfer/ConfirmDetails/ConfirmDetails.tsx
@@ -6,7 +6,7 @@ import Button from "components/Button";
 import { LABELS } from "constant";
 
 import styles from "./ConfirmDetails.styles";
-import DetailsListItem from "./ConfirmDetailsItem";
+import ConfirmDetailsItem from "./ConfirmDetailsItem";
 
 interface Props {
 	name: string;
@@ -24,19 +24,19 @@ const ConfirmDetails: React.FC<Props> = (props) => {
 		<View style={styles.container}>
 			<Text.Heading title={LABELS.TRANSFER_TITLE} />
 			
-			<DetailsListItem key={LABELS.TRANSFER_TO} label={LABELS.TRANSFER_TO} value={name} rollNo={rollNo} />
+			<ConfirmDetailsItem key={LABELS.TRANSFER_TO} label={LABELS.TRANSFER_TO} value={name} rollNo={rollNo} />
 			
 			<View style={styles.amountTaxContainer}>
 				<View style={styles.halfBox}>
-					<DetailsListItem key={LABELS.TRANSFER_AMOUNT} label={LABELS.TRANSFER_AMOUNT} value={amount} />
+					<ConfirmDetailsItem key={LABELS.TRANSFER_AMOUNT} label={LABELS.TRANSFER_AMOUNT} value={amount} />
 				</View>
 				<View style={styles.halfBox}>
-					<DetailsListItem key={LABELS.TRANSFER_TAX} label={LABELS.TRANSFER_TAX} value={tax} />
+					<ConfirmDetailsItem key={LABELS.TRANSFER_TAX} label={LABELS.TRANSFER_TAX} value={tax} />
 				</View>
 			</View>
 
 			<View>
-				<DetailsListItem key={LABELS.TRANSFER_FINAL_AMOUNT} label={LABELS.TRANSFER_FINAL_AMOUNT} value={totalAmount} />
+				<ConfirmDetailsItem key={LABELS.TRANSFER_FINAL_AMOUNT} label={LABELS.TRANSFER_FINAL_AMOUNT} value={totalAmount} />
 			</View>
 			
 			<Button title={LABELS.TRANSFER_BUTTON} onPress={onPressConfirmTransfer} />
