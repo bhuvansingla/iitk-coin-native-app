14ab9e83cd8fe32d39c851bb1d02f86dd1742f29
Harshit Raj <harshitr20@iitk.ac.in>
2021-10-16 01:05:16 +0530

    fix password regex

--- src/utils/validator/fields/password.ts
+++ src/utils/validator/fields/password.ts
@@ -4,9 +4,10 @@ function validate(password: string): boolean {
 	// atleast one number, one lowercase, one uppercase letter,
 	// one special character, atleast 8 characters, and atmax 16 characters
 	const regex = new RegExp(
-		`^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{${LENGTH.MIN.PASSWORD},${LENGTH.MAX.PASSWORD}}$`
+		`^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&+*-_]).{${LENGTH.MIN.PASSWORD},${LENGTH.MAX.PASSWORD}}$`
 	);
-	return regex.test(password);
+	const exclude = new RegExp ("[\"',./:;<>]");
+	return regex.test(password) && !exclude.test(password);
 }
 
 export default validate;
